5956fa86585e00d5f42683b343db6e952a701d70
Chen Reuven <chen.rvn@gmail.com>
2022-07-01 15:12:10 +0300

    fb: attach redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import 'tachyons';
+import {createStore} from "redux";
+import {searchRobotsReducer} from "./reducer";
+import {Provider} from "react-redux";
 
+const store = createStore(searchRobotsReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 // registerServiceWorker();
